a54d74798f9b64adf10234ed1389b4178bc4d6c6
Angela Carraro <angela_carraro@hotmail.it>
2019-11-17 18:10:41 +0100

    01-04_units_of_measure UPDATED

--- exercises/c++/01_intro/04_units_of_measure.cc
+++ exercises/c++/01_intro/04_units_of_measure.cc
@@ -1,5 +1,13 @@
 #include <iostream>
 
+constexpr double inch2meter{0.0254};
+constexpr double meter2inch{1./inch2meter};
+constexpr int atm2Pa{101325};
+constexpr int bar2Pa{100000};
+constexpr double Bohr2meter{0.529177208*1e-10};
+constexpr double ounces2liter{0.0295735};
+
+
 int main(){
 	
 	int a;
@@ -10,15 +18,15 @@ int main(){
 	std::cout << "The number according to the SI units is:" << std::endl;
 	if(strg == "inch")
 	{
-		std::cout << a * 0.0254 << " m" << std::endl;
+		std::cout << a * inch2meter << " m" << std::endl;
 	} else if (strg == "atm") {
-		std::cout << a*101325 << " Pa" << std::endl;
+		std::cout << a*atm2Pa << " Pa" << std::endl;
 	} else if (strg == "bar") {
-		std::cout << a*100000 << " Pa" << std::endl;
+		std::cout << a*bar2Pa << " Pa" << std::endl;
 	}else if (strg == "Bhor") {
-		std::cout << a*0.529177208*1e-10<<" m"<< std::endl;
-	} else if (strg == "oz") { //oz = once
-		std::cout << a*0.0295735<<" l"<< std::endl;
+		std::cout << a*Bohr2meter<<" m"<< std::endl;
+	} else if (strg == "oz") { //oz = ounces = once
+		std::cout << a*ounces2liter<<" l"<< std::endl;
 	} else {
 		std::cout << "Unit of measure not supported for the conversion" << std::endl;
 	}
