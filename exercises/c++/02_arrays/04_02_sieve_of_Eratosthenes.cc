9c51b5c1dae0f7554b3eec7c6bb768985835578c
Angela Carraro <angela_carraro@hotmail.it>
2019-11-17 18:38:23 +0100

    02-04_02_sieve_of_Eratosthenes UPDATED

--- exercises/c++/02_arrays/04_02_sieve_of_Eratosthenes.cc
+++ exercises/c++/02_arrays/04_02_sieve_of_Eratosthenes.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <algorithm> //for std::remove
 
 int main(){
 
@@ -14,19 +15,14 @@ int main(){
 	}
 
 	std::cout << "The prime numbers less than " << n << " are:" << std::endl;
-	for(std::size_t k{2}; k < ((int)std::sqrt(n)) + 1; ++k) {
-		if (primes.at(k - 2) != 0) {
-			for(std::size_t z{k}; k*z - 2 < primes.size(); ++z) {
-				primes.at(k*z - 2) = 0;
-			}
+	for(std::size_t k{2}; k < ((std::size_t)std::sqrt(n)) + 1; ++k) {
+		for(std::size_t z{k}; k*z - 2 < primes.size(); ++z) {
+			primes.erase(std::remove(primes.begin(), primes.end(), k*z), primes.end());
 		}
 	}
 
 	for(std::size_t k{0}; k < primes.size(); ++k) {
-		if (primes.at(k) != 0)
 			std::cout << primes.at(k) << std::endl;
-		//else
-			//primes.erase(primes.begin()+k);
 	}
 
 	return 0;
