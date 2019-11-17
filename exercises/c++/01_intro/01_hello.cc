393109892086c93c75f0199c64584816c146953f
Angela Carraro <angela_carraro@hotmail.it>
2019-11-17 18:03:12 +0100

    01-01_hello

---
+++ exercises/c++/01_intro/01_hello.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Hello, World!" << std::endl;
+	//std::c0ut << "Hello, World!" << std::endl;
+	int a = 4;
+	//std::cout << a1 << std::endl;	
+	//std::cout << a << std::endl
+	//int double{0};
+}
