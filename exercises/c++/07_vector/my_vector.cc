edf4ae7aee352a90deb2ea53e6e4470d1c8c7c18
Angela Carraro <angela_carraro@hotmail.it>
2019-11-27 22:21:06 +0100

    Studied exercise 07 - done in class

--- exercises/c++/07_vector/my_vector.cc
+++ exercises/c++/07_vector/my_vector.cc
@@ -23,15 +23,15 @@ class Vector {
 
 	void check_and_increase_capacity(){
 		if (_capacity == 0)
-			reserve(8); // reserve(1); -> memory allocation is expensive! At the beginning in thei case I have to make a lot of re-allocations!
+			reserve(8); // reserve(1); -> memory allocation is expensive! At the beginning in this case I have to make a lot of re-allocations!
 
 		if (_size == _capacity)
-			reserve(2 * _size); // we postpone the creation of this function (don't do today what you can do tomorrow)
+			reserve(2 * _size); // we postpone the creation of this function (don't do today what you can do tomorrow!)
 	}
 
 public:
-	auto size() const noexcept { return _size; }  // I'm returning by value so there is no possibility that the user modity the value of _size
-	auto capacity() const noexcept { return _capacity; }  // const is a stataement that I will not modity the value of the variable inside the function
+	auto size() const noexcept { return _size; }  // I'm returning by value so there is no possibility that the user modify the value of _size
+	auto capacity() const noexcept { return _capacity; }  // const is a statement that I will not modify the value of the variable inside the function
 
 	// here I can change the value of the vector
 	T& operator[](const std::size_t i) noexcept{ // this function DOESN'T check the boundaries of the array!
@@ -40,7 +40,7 @@ public:
 	}
 
 	// here I can only read the value of the element!
-	// T& operator[](const std::size_t i) const noexcept { -> here the compiler check that in the function you don't modify the value of the element, but the function returns a reference so you can modfy the value of the element
+	// T& operator[](const std::size_t i) const noexcept { -> here the compiler check that in the function you don't modify the value of the element, but the function returns a reference so you can modfy the value of the element: NONSENSE!
 	const T& operator[](const std::size_t i) const noexcept { // this function DOESN'T check the boundaries of the array!
 		// preconditions: elem != nullptr and i < _size
 		return elem[i];
@@ -59,7 +59,7 @@ public:
 
 	// we should also make a constructor that takes two values: the size and a value to put in all the elements of the vector
 	
-	// here there is no need of a reference (also in the custom ctor but it's a convention)
+	// here there is no need of a reference & (also in the custom ctor but it's a convention)
 	void reserve(const std::size_t new_size){ // public because if by chance you know in advance the size of your array you can reserve the memory immediately
 		if(_capacity < new_size) {
 			// for this line std::Vector is better that ours! For the built-in types are equivalent, but for type Data will call the default constructor of every element
@@ -82,22 +82,21 @@ public:
 
 	void push_back(T&& x){ // since c++11
 		check_and_increase_capacity();
-		elem[_size] = std::move(x); // coll the move semantic of your type if it exists, otherwise passes to copy
+		elem[_size] = std::move(x); // call the move semantic of your type if it exists, otherwise passes to copy
 		++_size;
 	}
+	
 	/* // With this function you don't need the other two!!!
 	template <typename OT>
 	void push_back(OT&& x){ // forwarded reference -> you have to put another template! 
 		check_and_increase_capacity();
-		elem[_size] = std::forward<OT>(x); // coll the move semantic of your type if it exists, otherwise passes to copy
+		elem[_size] = std::forward<OT>(x);
 		++_size;
 	} */
 
 	template <typename... Types> // variadic templates: it can be anything! -> your're packing all the variables type into one!
 	void emplace_back(Types&&... args) { // args contains all the arguments I have of that type
-		// doesn't work!
-		// elem[_size] = T{std::forward<Types>(args)...};  // I'm accepting all the types by value so I'm using a forwarded reference -> and I'm also using the move semantic
-		elem[_size] = T{std::forward<Types>(args)...};
+		elem[_size] = T{std::forward<Types>(args)...};  // I'm accepting all the types by value so I'm using a forwarded reference -> and I'm also using the move semantic
 		++_size;
 	}
 };
@@ -113,7 +112,8 @@ public:
 
 		return os;
 	}
-
+	
+	// for printing std::vector since we overloaded it
 	template <typename T>
 	std::ostream& operator <<(std::ostream& os, const std::vector<T>& x){
 		os << "size " << x.size() << "\n"
@@ -130,7 +130,7 @@ struct Date{
 	int d, m, y;
 	// the compiler creates default constructor, copy ctor, copy semantic, move ctor, move semantic
 	Date() { std::cout << "default ctor" << std::endl;}
-	Date(int d, int m, int y): d{d}, m{m}, y{y} {} // there is no confusion between the two d, m, y!
+	Date(int d, int m, int y): d{d}, m{m}, y{y} {} // there is no confusion between the two d's, m's and y's!
 };
 
 
